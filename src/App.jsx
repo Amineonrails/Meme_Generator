8d73d8944e5abaaa6200f34d3eb84d7605ea4a37
Amine <hatredisbae@gmail.com>
2022-08-27 12:31:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [meme, setMeme] = useState()
+
+
+  const getMeme = async () => {
+    const req = await axios.get('https://meme-api.herokuapp.com/gimme');
+    const memeUrl = req.data.url;
+    console.log(memeUrl)
+    setMeme(memeUrl)
+  }
+
+
+  useEffect(() => {
+
+    getMeme()
+
+  }, [])
+
+  return (
+    <div className="App">
+      <div>
+        <img src={meme} alt="meme" className='memeImage' />
+        <button onClick={() => { getMeme() }}>Generate a meme</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
